5cb23bc5c41d5588ef57783e81dd28f913d6719b
Jung <tht1234551@gmail.com>
2021-06-11 22:18:06 +0900

    defalte

---
+++ src/test/java/org/zerock/controller/JDBCTests.java
@@ -0,0 +1,33 @@
+package org.zerock.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try (Connection con =
+				DriverManager.getConnection(
+						"jdbc:oracle:thin:@localhost:1521:XE",
+						"book_ex",
+						"book_ex")) {
+			log.info(con);
+		} catch (Exception e) {
+			Assert.fail(e.getMessage());
+		}
+	}
+}
